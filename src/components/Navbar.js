749ab5bb2c6fe25690fa840beef7f17ae8e1a98b
aashish <aashish.shrestha@aayulogic.com>
2023-04-26 14:20:47 +0545

    add css to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export default class Navbar extends PureComponent {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-dark text-white">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand  text-white" to="/">
             NewsAggregator
           </Link>
           <button
@@ -20,40 +20,47 @@ export default class Navbar extends PureComponent {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <div
+            className="collapse navbar-collapse d-flex flex-row-reverse"
+            id="navbarNavDropdown"
+          >
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link
+                  className="nav-link active mx-3  text-white"
+                  aria-current="page"
+                  to="/"
+                >
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/business">
+              <li className="nav-item mx-3">
+                <Link className="nav-link  text-white" to="/business">
                   Business
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/entertainment">
+              <li className="nav-item mx-3">
+                <Link className="nav-link  text-white" to="/entertainment">
                   Entertainment
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/health">
+              <li className="nav-item mx-3 text-white">
+                <Link className="nav-link  text-white" to="/health">
                   Health
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/science">
+              <li className="nav-item mx-3">
+                <Link className="nav-link  text-white" to="/science">
                   Science
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/sports">
+              <li className="nav-item mx-3">
+                <Link className="nav-link  text-white" to="/sports">
                   Sports
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/technology">
+              <li className="nav-item mx-3 ">
+                <Link className="nav-link  text-white" to="/technology">
                   Technology
                 </Link>
               </li>
